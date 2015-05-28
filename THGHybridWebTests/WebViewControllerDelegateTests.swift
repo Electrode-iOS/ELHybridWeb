7b2f85dd3f8c34e3b3ffcee561b0949f07c660b8
Angelo Di Paolo <angelod101@gmail.com>
2015-05-28 12:46:45 -0400

    add unit tests for web view controller delegate methods

---
+++ THGHybridWebTests/WebViewControllerDelegateTests.swift
@@ -0,0 +1,87 @@
+//
+//  WebViewControllerDelegateTests.swift
+//  THGHybridWeb
+//
+//  Created by Angelo Di Paolo on 5/20/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import THGHybridWeb
+
+class WebViewControllerDelegateTests: XCTestCase, WebViewControllerDelegate {
+    
+    var didStartLoadExpectation: XCTestExpectation?
+    var didFinishLoadExpectation: XCTestExpectation?
+    var shouldStartLoadExpectation: XCTestExpectation?
+    var didFailLoadExpectation: XCTestExpectation?
+    
+    func testDelegateDidStartLoad() {
+        let vc = WebViewController()
+        vc.delegate = self
+        
+        if let url = NSURL(string: "http://httpbin.org/") {
+            didStartLoadExpectation = expectationWithDescription("did start load")
+            
+            vc.loadURL(url)
+            
+            waitForExpectationsWithTimeout(4.0, handler: nil)
+        }
+    }
+    
+    func testDelegateDidFinishLoad() {
+        let vc = WebViewController()
+        vc.delegate = self
+        
+        if let url = NSURL(string: "http://httpbin.org/") {
+            didFinishLoadExpectation = expectationWithDescription("did finish load")
+            
+            vc.loadURL(url)
+            waitForExpectationsWithTimeout(4.0, handler: nil)
+        }
+    }
+    
+    func testDelegateShouldStartLoad() {
+        let vc = WebViewController()
+        vc.delegate = self
+        
+        if let url = NSURL(string: "http://httpbin.org/") {
+            shouldStartLoadExpectation = expectationWithDescription("should start load")
+            
+            vc.loadURL(url)
+            waitForExpectationsWithTimeout(4.0, handler: nil)
+        }
+    }
+    
+    func testDelegateDidFail() {
+        let vc = WebViewController()
+        vc.delegate = self
+        
+        if let url = NSURL(string: "http://foosomecrazydomainthatwillneverwork.org/somecrazypath") {
+            didFailLoadExpectation = expectationWithDescription("did fail load")
+            
+            vc.loadURL(url)
+            waitForExpectationsWithTimeout(4.0, handler: nil)
+        }
+    }
+    
+    // MARK: WebViewControllerDelegate
+    
+    func webViewControllerDidStartLoad(webViewController: WebViewController) {
+        didStartLoadExpectation?.fulfill()
+    }
+    
+    func webViewControllerDidFinishLoad(webViewController: WebViewController) {
+        didFinishLoadExpectation?.fulfill()
+    }
+    
+    func webViewController(webViewController: WebViewController, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        shouldStartLoadExpectation?.fulfill()
+        return true
+    }
+    
+    func webViewController(webViewController: WebViewController, didFailLoadWithError error: NSError) {
+        didFailLoadExpectation?.fulfill()
+    }
+}
