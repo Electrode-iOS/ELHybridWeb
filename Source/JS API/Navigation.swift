be01b458b75fe5250e22e71cf18dbf80fbf9769c
David Pettigrew <dpettigrew@walmartlabs.com>
2015-10-23 11:29:41 -0700

    popToRoot fix

--- Source/JS API/Navigation.swift
+++ Source/JS API/Navigation.swift
@@ -40,7 +40,7 @@ import JavaScriptCore
     func popToRoot() {
         THGHybridWebLogger.sharedLogger.log(.Debug, message: "") // provide breadcrumbs
         dispatch_async(dispatch_get_main_queue()) {
-            self.parentViewController?.navigationController?.popToRootViewControllerAnimated(false)
+            self.webViewController?.popToRootWebViewController(false)
         }
     }
 
